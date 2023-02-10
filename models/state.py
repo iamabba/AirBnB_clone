12ffe8c7293743b5f8fe52bd7926f2143bff12eb
christian ng'ang'a <111011919+themathematcs@users.noreply.github.com>
2023-02-10 19:48:17 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -6,8 +6,6 @@ from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state
-    Attributes:
-        name (str): The name of the state
+    """Represent a state Attributes name 
     """
     name = ""
